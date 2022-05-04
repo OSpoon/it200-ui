0f46dbac22168d222246dd42b8aae4327a6ea5c8
fe-xiaoxin <zxin088@gmail.com>
2022-05-04 22:29:10 +0800

    🐳 chore(remove): 移除组件文档

--- docs/.vuepress/clientAppEnhance.js
+++
@@ -1,6 +0,0 @@
-import { defineClientAppEnhance } from "@vuepress/client";
-import Card from "./components/card.vue";
-
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-  app.component(Card.name, Card);
-});
